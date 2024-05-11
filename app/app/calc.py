33c2cb0b9d261e57b4eb403caa16a359f6991108
Agustin Miller <agusmill@gmail.com>
2024-05-11 15:08:08 -0300

    Added simple tests examples, added posgres db config

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""Calculator functions
+    """
+
+def add(x,y):
+    "Add x and y return result"
+    return x+y
+    
+def substract(x,y):
+    "Substract x and y and return result"
+    return y-x
